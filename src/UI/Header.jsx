47d6e5c73689ecbd3a93264fd3349f5edbfcc2e5
yymengduuu <yymengduuu@gmail.com>
2025-07-06 22:45:55 +0100

    update css

--- src/UI/Header.jsx
+++ src/UI/Header.jsx
@@ -1,8 +1,11 @@
 export default function Header() {
   return (
-    <header>
-      <p>fast react pizza co. </p>
-      <input placeholder="Search order # " />
+    <header className="bg-orange-400 flex items-center justify-between px-4 py-3">
+      <p className="uppercase ">fast react pizza co. </p>
+      <input
+        className="bg-white rounded-full text-stone-100 text-center"
+        placeholder="Search order # "
+      />
     </header>
   );
 }
--- src/UI/Home.jsx
+++ src/UI/Home.jsx
@@ -1,10 +1,26 @@
 export default function Home() {
   return (
-    <div>
-      <h1>The best pizza .</h1>
-      <h2>Straight out of the oven, straight to you.</h2>
-      <p>👋 Welcome! Please start by telling us your name:</p>
-      <input placeholder="Your full name" />
+    <div
+      className="text-center w-full py-16 px-4 font-serif min-h-screen bg-cover bg-center bg-no-repeat"
+      style={{
+        backgroundImage: "url('/images/bg-pizza.jpg')",
+        backgroundColor: "rgba(255,255,255,0.85)",
+        backgroundBlendMode: "lighten",
+      }}
+    >
+      <h1 className="mb-24  text-5xl md:text-5xl text-gray-900 drop-shadow-xl font-bold">
+        The best pizza .
+      </h1>
+      <h2 className="mb-16 text-3xl text-gray-700 font-semibold">
+        Straight out of the oven, straight to you.
+      </h2>
+      <p className="mb-16 text-lg text-gray-800 font-medium">
+        👋 Welcome! Please start by telling us your name:
+      </p>
+      <input
+        className="mb-4 rounded-full text-stone-100 text-center px-4 py-2 border-gray-900"
+        placeholder="Your full name"
+      />
     </div>
   );
 }
