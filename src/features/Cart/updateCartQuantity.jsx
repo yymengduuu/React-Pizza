8595b58dd2dc6e01c436c9629006bc7e358d209c
yymengduuu <yymengduuu@gmail.com>
2025-07-15 17:21:02 +0100

    update maxquantity

--- src/features/Cart/updateCartQuantity.jsx
+++ src/features/Cart/updateCartQuantity.jsx
@@ -15,6 +15,7 @@ export default function UpdateCartQuantity({ id, quantity }) {
       <span className="mx-3">{quantity}</span>
       <button
         className="rounded-full bg-orange-400 px-2 py-0.5 text-sm text-black hover:bg-orange-500 md:px-3.5 md:py-2"
+        disabled={quantity >= 10}
         onClick={() => dispatch(increaseItemQuantity(id))}
       >
         +
